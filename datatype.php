ae85f53dfe9ed074272b860e17136bcdf53fd0b9
nisehniheng <s64309010029@ptnicec.ac.th>
2022-10-20 12:16:42 +0700

    ส่ง

---
+++ datatype.php
@@ -0,0 +1,27 @@
+<?php
+
+$grade = "H";
+$oject = "html";
+$name = "seh";
+$age = "22";
+
+//integer Numbwr
+$timeout = 30;
+$score = 99;
+$loan = -99;
+$money = +99;
+
+//Floating
+$gpa = 3.33;
+$inits = 4.33;
+$temp = -5.33;
+$lumem = +126.33;
+$maths = 2e10;
+
+//Boolean
+$famale = true;
+$male = false;
+
+echo $maths;
+
+?>
---
+++ function_sting.php
@@ -0,0 +1,22 @@
+<?php
+
+    $name = "gnehin hesin";
+    echo strlen($name);
+    echo "<br>";
+    $word = "I Love you";
+    echo str_word_count($word);
+    echo "<br>";
+    echo strrev($name);
+    echo "<br>";
+    echo strpos($word,"love");
+    echo "<br>";
+    echo str_repeat("love","hate",$word);
+    echo strtoupper($word);
+    echo "<br>";
+    echo strtolower( $name);
+    echo "<br>";
+    echo substr($word,-3);
+    echo "<br>";
+    echo substr($word,+2,-4);
+
+?>
