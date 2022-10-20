b10b116526d5e73527d9bb053c485d6e0fcdc68e
nisehniheng <s64309010029@ptnicec.ac.th>
2022-10-20 11:13:54 +0700

    ไฟล์ variable/test

--- test.php
+++ test.php
@@ -1,3 +1,31 @@
 <?php 
 
+echo "pim<br>";
+//echo "Thalan";
+#echo "Asia";
+/*
+ควยเยอะ
+echo "world<br>";
+echo "Universe<br>";
+*/
+
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     information <br>
+     <?php
+         echo "pim";
+
+     ?>
+</body>
+</html>
+
+
---
+++ variabie.php
@@ -0,0 +1,19 @@
+<?php
+
+   $fname = 2;
+   $a111 = 1;
+   $_test = 2.33;
+   $Age = 21;
+   $age = 24;
+   $fullname = "niseh";
+   $prefix = "Mr.";
+
+   echo $fname."<br>";
+   echo $a111;
+   echo "<br>".$Age;
+   echo "<br>". $prefix.$fullname."<br>";
+   echo $Age + $age."<br>" ;
+   echo $Age." + ".$age."<br>" ;
+   echo $Age." + ".$age. "=" .($Age + $age)."<br>" ;
+
+?>
